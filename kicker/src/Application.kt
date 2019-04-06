2ed775d0d0f1dadbcef60e24ac3ccc74b69e8e8e
Ксения Гонта <gontakseniya@gmail.com>
2019-04-06 14:56:06 +0300

    Added simple login

--- kicker/src/Application.kt
+++ kicker/src/Application.kt
@@ -1,18 +1,22 @@
 package com.kicker
 
+import com.kicker.Controllers.UserController
 import com.kicker.Controllers.gameController
 import com.kicker.Controllers.statsController
 import com.kicker.Controllers.userController
+import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
+import io.ktor.freemarker.FreeMarker
+import io.ktor.response.respondRedirect
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
+
 fun main(args: Array<String>) {
+
     startServer()
     createDatabase()
 }
@@ -20,10 +24,16 @@ fun main(args: Array<String>) {
 
 fun startServer() {
     val server = embeddedServer(Netty, port = 8080) {
+        install(FreeMarker) {
+            templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        }
         routing {
             gameController()
             userController()
             statsController()
+            get("/") {
+                call.respondRedirect("/login", permanent = false)
+            }
         }
 
     }
--- kicker/src/Controllers/UserController.kt
+++ kicker/src/Controllers/UserController.kt
@@ -1,17 +1,40 @@
 package com.kicker.Controllers
 
 import io.ktor.application.call
+import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.request.receiveParameters
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.route
 
 const val REGISTER_ENDPOINT = "/register"
+const val LOGIN_ENDPOINT = "/login"
 
 class UserController {
+    fun validCredentials(username: String?, password: String?): Boolean {
+        return username != null && username == password
+    }
 }
 
 fun Route.userController() {
     get(REGISTER_ENDPOINT) {
         call.respondText("register")
+    }
+
+    route(LOGIN_ENDPOINT) {
+        get {
+            call.respond(FreeMarkerContent("login.ftl", null))
+        }
+        post {
+            val post = call.receiveParameters()
+            if (post["username"] != null && post["username"] == post["password"]) {
+                call.respondText("OK")
+            } else {
+                call.respond(FreeMarkerContent("login.ftl", mapOf("error" to "Invalid login")))
+            }
+        }
     }
 }
